06fce93572bab9c2b60d7b3381ef92918e1ec1b4
Rajkamal <110493358+rajk1101@users.noreply.github.com>
2024-11-11 15:12:55 +0530

    Multiple Route and next()method

--- src/app.js
+++ src/app.js
@@ -1,36 +1,26 @@
-const express=require("express");
+const express = require("express");
 
+const app = express(); // this is the instance of the express application; it creates a new web server
+//warapping the routes in an array does not affect anything 
+app.use(
+  "/user",
+  [(req, res,next) => {
+    console.log("Handling the Route User 1");
+    // res.send("1st Response");
+    next();  // if i will not send res from here then it will goes to next routehandler
+  },
+ [ (req, res,next) => {
+    console.log("Handling the Route User 2");
+    // res.send("2nd Response");
+    next();
+  },
+  (req, res,next) => {
+    console.log("Handling the Route User 3");
+    res.send("3nd Response");   
+    // next();
+  }]]
+);
 
-const app=express(); // this is the instance of the express js Applicaton  it is like creating a new web server
-
-// this will match the all http method API calls to /test/users 
-app.use("/test",(req,res)=>{        // handling the server
-    res.send("Hello test from the Server")
-})
- //this will only handle GET call   to /user/
-app.get("/user",(req,res)=>{ 
-    console.log(req.query   )
-    res.send({firstName:"Sudhir",lastName:"kumar"})
-})
-
-app.post("/user",(req,res)=>{
-    res.send("Data successfully saved to the database")
-})
-app.use("/delete",(req,res)=>{        // handling the server
-    res.send("Data Deleted Succesfully")
-})
-
-
-// app.get("/user/:userID/:name/:password",(req,res)=>{ 
-//     console.log(req.params )
-//     res.send({firstName:"Sudhir",lastName:"kumar"})
-// })
-
-// app.use("/hello",(req,res)=>{        // handling the server
-//     res.send("Hello from the Server")
-// })
-
-
-app.listen(3000,()=>{
-    console.log("Server is successfully listening on port 3000")
-});    // server is listening to us 
+app.listen(3000, () => {
+  console.log("Server is successfully listening on port 3000");
+});
